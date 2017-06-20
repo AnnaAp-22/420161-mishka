8bcd88365a98dfd2b72558447c18c293af28664a
Anna <post-a22@mail.ru>
2017-06-20 23:53:58 +0300

    Подготовила адаптивную графику в личном проекте.

--- gulpfile.js
+++ gulpfile.js
@@ -5,22 +5,36 @@ var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var rename = require("gulp-rename");
+var svgstore = require("gulp-svgstore");
+var svgmin = require("gulp-svgmin");
 var server = require("browser-sync").create();
 
-gulp.task("style", function() {
+gulp.task("style", function () {
   gulp.src("sass/style.scss")
     .pipe(plumber())
     .pipe(sass())
     .pipe(postcss([
-      autoprefixer({browsers: [
-        "last 2 versions"
-      ]})
+      autoprefixer({
+        browsers: [
+          "last 2 versions"
+        ]
+      })
     ]))
     .pipe(gulp.dest("css"))
     .pipe(server.stream());
 });
+gulp.task("symbols", function () {
+  return gulp.src("img/icons/*.svg")
+    .pipe(svgmin())
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("symbols.svg"))
+    .pipe(gulp.dest("img"));
+});
 
-gulp.task("serve", ["style"], function() {
+gulp.task("serve", ["style"], function () {
   server.init({
     server: ".",
     notify: false,
