a791cfb259c498d696fea7803742104d145a208f
Anna <post-a22@mail.ru>
2017-06-21 21:31:05 +0300

    Добавила модальное окно и Google-карту.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+var map = document.querySelector(".contacts__map");
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -10,5 +11,30 @@ navToggle.addEventListener('click', function () {
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+  }
+});
+
+map.classList.remove("contacts__map--nojs");
+
+var cartlink = document.querySelectorAll(".product-best__order");
+var cartPopup = document.querySelector(".modal");
+var cartClose = cartPopup.querySelector(".modal__close");
+//
+//корзина
+[].forEach.call(cartlink, function (cartlink) {
+  cartlink.addEventListener("click", function (event) {
+    event.preventDefault();
+    cartPopup.classList.add("modal__content-show");
+  });
+});
+cartClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  cartPopup.classList.remove("modal__content-show");
+});
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (cartPopup.classList.contains("modal__content-show")) {
+      cartPopup.classList.remove("modal__content-show");
+    }
   }
 });
